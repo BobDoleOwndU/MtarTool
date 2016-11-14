c667d103cb60ee7e21c7b3069a335d6b597539eb
BobDoleOwndU <spikey5556@hotmail.com>
2016-11-14 17:35:14 -0500

    Initial commit.

---
+++ MtarTool/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MtarTool
+{
+    class Program
+    {
+        struct file
+        {
+            string name;
+            int offset;
+            int length;
+        } //struct file ends
+
+        static int fileNameOffset = 0x20 * 0x2;
+        static int fileOffset;
+
+        List<file> fileList = new List<file>(0);
+
+        static void Main(string[] args)
+        {
+            GetFileInfo();
+        } //function Main ends
+
+        static void GetFileInfo()
+        {
+            string path = @"C:\Program Files (x86)\Steam\steamapps\common\MGS_TPP\master\chunk0_dat\Assets\tpp\pack\player\motion\player2_facial_snake_fpk\Assets\tpp\motion\mtar\player2\TppPlayer2Facial.mtar";
+            bool run = true;
+            
+            var file = File.ReadAllBytes(path);
+            string hex = BitConverter.ToString(file).Replace("-", String.Empty);
+
+            while(run)
+            {
+                file File1;
+
+                if(hex.Substring((fileNameOffset + 0x8) * 0x2, 0x1 * 0x2) != "FC")
+                {
+                    Console.WriteLine(hex.Substring(fileNameOffset * 0x2 + 0x8 * 0x2, 0x1 * 0x2));
+                    Console.WriteLine("End of file section");
+                    run = false;
+                } //if ends
+                else
+                {
+                    Console.WriteLine(hex.Substring(fileNameOffset, 0x7 * 0x2));
+                }
+            } //while ends
+        } //function GetFileNames ends
+    } //class Program ends
+} //namespace MtarTool ends
